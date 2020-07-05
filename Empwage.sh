cde88c316c7fb73749b8b76f32a8b008882593c1
sawantpalvi1234 <sawantpalvi087@gmail>
2020-07-06 01:31:06 +0530

    committed

--- Empwage.sh
+++ Empwage.sh
@@ -2,13 +2,24 @@
 
 echo "WELCOME EMPLOYEE"
 
+HOUR=8
+WAGE=20
+DAILYWAGE=0
+
+dailywage(){
+        DAILYWAGE=$(($WAGE * $HOUR))
+        echo "Daily Employee Wage is= $DAILYWAGE"
+}
+
 attendance(){
 	R=$((RANDOM % 2))
 	if [ $R -eq 1 ]
 		then
 			echo "Employee is Present"
+			dailywage
 		else
 			echo "Employee is Absent"
 	fi
 }
 attendance
+
