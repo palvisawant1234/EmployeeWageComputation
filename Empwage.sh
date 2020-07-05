d3ec6cf037684f82909751abeaa2d89ee28eea43
sawantpalvi1234 <sawantpalvi087@gmail>
2020-07-06 02:22:20 +0530

    committed

--- Empwage.sh
+++ Empwage.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 echo "WELCOME EMPLOYEE"
 
@@ -6,20 +6,12 @@ HOUR=8
 WAGE=20
 DAILYWAGE=0
 PARTTIME=0
-
-attendance(){
-	R=$((RANDOM % 2))
-	if [ $R -eq 1 ]
-		then
-			echo "Employee is Present"
-		else
-			echo "Employee is Absent"
-	fi
-}
+TOTALDAYS=20
+TOTALWAGE=0
 
 dailywage(){
-	DAILYWAGE=$(($WAGE * $HOUR))
-	echo "Daily Employee Wage is= $DAILYWAGE"
+        DAILYWAGE=$(($WAGE * $HOUR))
+        echo "Daily Employee Wage is= $DAILYWAGE"
 }
 
 partwage(){
@@ -28,13 +20,29 @@ partwage(){
 	echo "Part time Employee Wage is $PARTTIME"
 }
 
-echo -e "1.Attendance\n2.Daily Wage\n3.Part Time Wage\n4.Exit" 
-read ch
-case $ch in
-	"1")attendance;;
-	"2")dailywage;;
-	"3")partwage;;
-	"4")exit;;
-	"*")echo Invalid choice
-esac
+attendance(){
+	R=$((RANDOM % 2))
+	if [ $R -eq 1 ]
+		then
+			RD=$((RANDOM % 2))
+		        if [ $RD -eq 1 ]
+                	then
+                        	dailywage
+				TOTALWAGE=$(($DAILYWAGE+$TOTALWAGE))
+                	else
+                        	partwage
+				TOTALWAGE=$(($PARTTIME+$TOTALWAGE))
+        		fi
+		else
+			echo "Employee is Absent"
+	fi
+}
+
+for ((i=1 ; i<= $TOTALDAYS ;i++))
+do
+	echo -n "day $i = "
+	attendance
+done
+
+echo "Total wage of a month=$TOTALWAGE"
 
