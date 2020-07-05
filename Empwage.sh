3ef1d1acc196efe8ce929794c3b9e3442ffba9ce
sawantpalvi1234 <sawantpalvi087@gmail>
2020-07-06 01:53:37 +0530

    committed

--- Empwage.sh
+++ Empwage.sh
@@ -5,34 +5,36 @@ echo "WELCOME EMPLOYEE"
 HOUR=8
 WAGE=20
 DAILYWAGE=0
-
-dailywage(){
-        DAILYWAGE=$(($WAGE * $HOUR))
-        echo "Daily Employee Wage is= $DAILYWAGE"
-}
-
 PARTTIME=0
-partwage(){
-	TIME=$(($HOUR / 2))
-	PARTTIME=$(($TIME * $WAGE))
-	echo "Part time Employee Wage is $PARTTIME"
-}
 
 attendance(){
 	R=$((RANDOM % 2))
 	if [ $R -eq 1 ]
 		then
 			echo "Employee is Present"
-			RD=$((RANDOM % 2))
-		        if [ $RD -eq 1 ]
-                	then
-                        	dailywage
-                	else
-                        	partwage
-        		fi
 		else
 			echo "Employee is Absent"
 	fi
 }
-attendance
+
+dailywage(){
+	DAILYWAGE=$(($WAGE * $HOUR))
+	echo "Daily Employee Wage is= $DAILYWAGE"
+}
+
+partwage(){
+	TIME=$(($HOUR / 2))
+	PARTTIME=$(($TIME * $WAGE))
+	echo "Part time Employee Wage is $PARTTIME"
+}
+
+echo -e "1.Attendance\n2.Daily Wage\n3.Part Time Wage\n4.Exit" 
+read ch
+case $ch in
+	"1")attendance;;
+	"2")dailywage;;
+	"3")partwage;;
+	"4")exit;;
+	"*")echo Invalid choice
+esac
 
